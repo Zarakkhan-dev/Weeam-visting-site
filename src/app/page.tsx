ab0cd3aca6bffaeccb9842898fb70b1091568c4d
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-06-16 10:05:20 +0500

    Added package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { Metadata } from 'next'
 import Hero from './components/Home/Hero'
-import Company from './components/Home/Company'
+// import Company from './components/Home/Company'
 import Records from './components/Home/Records'
 import Service from './components/Home/Service'
-import AboutUs from './components/Home/AboutUs'
 import Network from './components/Home/Network'
 import Review from './components/Home/Review'
 import ContactForm from './components/Contact/Form'
 import Newsletter from './components/Home/Newsletter/Newsletter'
 import MainService from './components/Home/MainService'
 import WebsiteInterface from './components/Home/WebsiteInterface'
+import AboutUs from './components/Home/Aboutus'
 
 export const metadata: Metadata = {
   title: 'weeam.',
